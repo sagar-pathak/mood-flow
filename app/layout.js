6ea64eaef6771e6f8683e6c428c09b13ff15da5d
Sagar Pathak <sagarpaathak@gmail.com>
2024-09-05 01:46:59 -0500

    name change

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   const header = (
     <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
       <Link href={'/'}>
-        <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
+        <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>MoodFlow</h1>
       </Link>
       <Logout />
     </header>
